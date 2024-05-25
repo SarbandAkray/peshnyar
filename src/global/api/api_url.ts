035b9b63654877f3bc183b8456c82b6d2ff0000b
Sarband <sarbandmuhamad@gmail.com>
2024-05-26 02:18:14 +0300

    http to https fixed

--- src/global/api/api_url.ts
+++ src/global/api/api_url.ts
@@ -1,3 +1,3 @@
-export const baseApiUrl = "http://api.peshnyar.com/api/v1/";
-export const baseApiImageUrl = "http://api.peshnyar.com/api/v1";
-export const baseBackendUrl = "http://api.peshnyar.com";
+export const baseApiUrl = "https://api.peshnyar.com/api/v1/";
+export const baseApiImageUrl = "https://api.peshnyar.com/api/v1";
+export const baseBackendUrl = "https://api.peshnyar.com";
