fb16bb46f60194fbc900a0ac44009d96026c9196
SarbandAkray <sarband2001muhamad@gmail.com>
2024-06-25 23:44:24 +0300

    both url added live and local

--- src/global/api/api_url.ts
+++ src/global/api/api_url.ts
@@ -1,3 +1,9 @@
+// live urls
 export const baseApiUrl = "https://api.peshnyar.com/api/v1/";
 export const baseApiImageUrl = "https://api.peshnyar.com/api/v1";
 export const baseBackendUrl = "https://api.peshnyar.com";
+
+// localhost
+// export const baseApiUrl = "http://localhost:3000/api/v1/";
+// export const baseApiImageUrl = "http://localhost:3000/api/v1";
+// export const baseBackendUrl = "http://localhost:3000";
